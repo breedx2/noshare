9c63a35d0c4f4e0d8eb77fdf25297fcdd7502ca9
jason <jason@noisybox.net>
2022-03-22 22:32:47 -0700

    remove ancient adhoc test scripts

--- client/client-ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh -p 20666 \
-	-o "StrictHostKeyChecking=no" \
-	-o "UserKnownHostsFile=/dev/null" \
-	-i /home/jason/noshare/id_rsa app@localhost
--- client/tunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#-L 6666:google.com:2222 \
-ssh -p 20666 \
-	-o "StrictHostKeyChecking=no" \
-	-o "UserKnownHostsFile=/dev/null" \
-	-N \
-	-L 6666:localhost:2222 \
-	-i /home/jason/noshare/id_rsa app@localhost
