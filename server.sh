be13200b2e8de24c3048e046a0c2cfb5e860f252
jason <jason@noisybox.net>
2022-09-03 17:31:46 -0700

    add ability to specify hostkeys

--- server.sh
+++ server.sh
@@ -14,6 +14,17 @@ if [ "${NOSHARE_KEYS_DIR}" == "" ] ; then
 	echo "Defaulting keys dir to ${NOSHARE_KEYS_DIR}"
 fi
 
+if [ "${NOSHARE_HOST_KEYS_DIR}" == "" ] ; then
+	if [ -d "${MYDIR}/ssh_host_keys" ] ; then
+		echo "Found ${MYDIR}/ssh_host_keys, using as server ssh host keys"
+		NOSHARE_HOST_KEYS_DIR="${MYDIR}/ssh_host_keys"
+	else
+		echo "NOSHARE_HOST_KEYS_DIR not specified and default not found"
+		echo "New host keys will be generated on container start."
+		echo "* Users may need to update their server fingerprint!"
+	fi
+fi
+
 MOUNT_DIR="${NOSHARE_KEYS_DIR}/mounted"
 
 rm -rf "${MOUNT_DIR}"
@@ -29,9 +40,15 @@ for f in $(find "${NOSHARE_KEYS_DIR}" -type f -name '*.pub') ; do
 	cat $f >> "${MOUNT_DIR}/$FN"
 done
 
+if [ "${NOSHARE_HOST_KEYS_DIR}" != "" ] ; then
+	HOSTKEYS_PREFIX="-v"
+	HOSTKEYS_PART="${NOSHARE_HOST_KEYS_DIR}:/config/ssh_host_keys"
+fi
+
 docker run -d --name noshare --restart=on-failure:5 \
         -p ${NOSHARE_PORT}:2222 \
         -v "${MOUNT_DIR}:/etc/pubkeys" \
+		"${HOSTKEYS_PREFIX}" "${HOSTKEYS_PART}" \
         -e DOCKER_MODS=linuxserver/mods:openssh-server-ssh-tunnel \
         -e PUBLIC_KEY_DIR=/etc/pubkeys \
         -e USER_NAME=app \
